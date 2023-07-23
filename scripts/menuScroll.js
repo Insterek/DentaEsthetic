ef00b457e179a9ddb8910c73c358a1e92f096b4b
insterek <damian2000mankiewicz@o2.pl>
2023-07-23 13:40:43 +0200

    change section class to id,add menuScroll.js scroll into section

---
+++ scripts/menuScroll.js
@@ -0,0 +1,11 @@
+const links = document.querySelectorAll(".button");
+links.forEach((item) => {
+  item.addEventListener("click", () => {
+    const element = document.getElementById(item.getAttribute("data-link"));
+    element.scrollIntoView({ behavior: "smooth", block: "end" });
+  });
+});
+window.addEventListener("scroll", () => {
+  const navigation = document.querySelector(".navigation");
+  navigation.classList.toggle("scroll", scrollY > 150);
+});
