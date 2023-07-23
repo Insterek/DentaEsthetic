03f08eee45aa534a28321f04234116d1f5e5c7fc
insterek <damian2000mankiewicz@o2.pl>
2023-07-23 14:31:40 +0200

    add README.md and favicon

---
+++ README.md
@@ -0,0 +1,18 @@
+# DentaEsthetic
+
+Simple one page created with:
+
+- HTML
+- CSS
+- JavaScript
+- RWD
+
+## Download
+
+```
+git clone https://github.com/Insterek/DentaEsthetic.git
+```
+
+## Lighthouse test:
+
+![lighthuse test](images/others/lighthouse.jpg)
--- scripts/menuScroll.js
+++ scripts/menuScroll.js
@@ -3,6 +3,7 @@ links.forEach((item) => {
   item.addEventListener("click", () => {
     const element = document.getElementById(item.getAttribute("data-link"));
     element.scrollIntoView({ behavior: "smooth", block: "end" });
+    closeMenu();
   });
 });
 window.addEventListener("scroll", () => {
